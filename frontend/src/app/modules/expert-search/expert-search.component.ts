544647a07a771876ed21378eb0ecc0949d0f5f53
Aron Schnakenbeck <aron.schnakenbeck@hsu-hh.de>
2021-07-14 15:56:39 +0200

    added sparql form

--- frontend/src/app/modules/expert-search/expert-search.component.ts
+++ frontend/src/app/modules/expert-search/expert-search.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import {Apollo, gql} from 'apollo-angular';
 
 @Component({
   selector: 'app-expert-search',
   templateUrl: './expert-search.component.html',
   styleUrls: ['./expert-search.component.css']
 })
+
+
 export class ExpertSearchComponent implements OnInit {
+  rates: any[];
+  loading = true;
+  error: any;
 
-  constructor() { }
+  constructor(private apollo: Apollo) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.apollo
+      .watchQuery({
+        query: gql`
+          {
+            rates(currency: "USD") {
+              currency
+              rate
+            }
+          }
+        `,
+      })
+      .valueChanges.subscribe((result: any) => {
+        this.rates = result?.data?.rates;
+        this.loading = result.loading;
+        this.error = result.error;
+      });
   }
 
 }
