84d330af4268b7a29db2fc1ad70ff8a004081ab3
Milapji <milapji.gill@hsu-hh.de>
2021-07-16 13:42:22 +0200

    Profil-Komponente mit Benutzerinformationen und Expertise Level

---
+++ frontend/src/app/modules/profil/profil.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profil',
+  templateUrl: './profil.component.html',
+  styleUrls: ['./profil.component.scss']
+})
+export class ProfilComponent implements OnInit {
+
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
