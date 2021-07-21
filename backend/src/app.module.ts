73d0a95aec7ddda97e2dc6957c5c890097bacacf
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2021-07-21 11:51:18 +0200

    Added entry for automated type generation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SparqlQueryModule } from './graphql/sparql-query.module';
@@ -10,6 +11,9 @@ import { BreakingBadQuoteModule } from './routes/breaking-bad-quotes/breaking-ba
 		GraphQLModule.forRoot({
 			debug: false,
 			typePaths: ['./src/graphql/schema.graphql'],
+			definitions: {
+				path: join(process.cwd(), './src/graphql/schema.ts'),
+			},
 			playground: true,
 		}),
 		BreakingBadQuoteModule,
