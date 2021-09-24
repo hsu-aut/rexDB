934862a8cf13064a4a97272e472024204c0faf80
Milapji <milapji.gill@hsu-hh.de>
2021-09-24 10:23:34 +0200

    Leichte Anpassungen des Profils

---
+++ frontend/src/app/modules/profil/profil.ts
@@ -0,0 +1,20 @@
+export class Profil {
+
+    constructor(
+        public name: string,
+        public position: string,
+        public hireDate: Date,
+        public mail: string,
+        public phone: number,
+        public room: string,
+        public github: string,
+        public projectName: string,
+        public projectStart: Date,
+        public projectEnd: Date,
+        public researchTopic: string,
+        public tuitionModel: string,
+        public studentProject:string
+
+        ) {}
+
+}
