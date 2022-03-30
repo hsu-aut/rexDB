e54a54ebac12f326733b994ceabbdbf814368e9f
Aron Schnakenbeck <aron.schnakenbeck@hsu-hh.de>
2022-03-30 15:13:51 +0200

    added FieldMap (to get graphQl query) and graphQLtoSparql library

--- backend/src/graphql/expert.resolver.ts
+++ backend/src/graphql/expert.resolver.ts
@@ -1,19 +1,44 @@
+//import { FieldMap } from "@jenyus-org/nestjs-graphql-utils";
+import { FieldMap as FieldMapType } from "@jenyus-org/graphql-utils";
+import { FieldMap } from "@jenyus-org/nestjs-graphql-utils";
 import { Args, Query, Resolver } from "@nestjs/graphql";
 import { GraphQLScalarType, Kind } from "graphql";
 import { Expert } from "./schema";
+const Converter = require('graphql-to-sparql').Converter;
+const { toSparql } = require('sparqlalgebrajs');
 
 @Resolver('Experts')
 export class ExpertResolver {
 	constructor() {}
 
   @Query()
-	async experts() {
-		console.log("resolver resolces expert");
+    //https://www.npmjs.com/package/graphql-fields
+	async experts(@FieldMap() fieldMap: FieldMapType,) { 
+		console.log("resolver resolves expert");
+        console.log(fieldMap);
 
+        //https://www.npmjs.com/package/graphql-to-sparql
+        const algebra = await new Converter().graphqlToSparqlAlgebra('{experts{firstName,lastName,expertise{name, level}}}', {
+            "experts": {"@type": "@id", "@id":"http://example.org/expert"}, //TODO: Typisieren von Experts fehlt. Wie geht das in JSON LD?
+            "firstName": "http://example.org/name",
+            "lastName": "http://example.org/friends",
+            "expertise": "http://example.org/friend",
+            "name": "http://example.org/frien",
+            "level": "http://example.org/friends"
+          });
+
+        console.log(algebra);
+        //https://www.npmjs.com/package/sparqlalgebrajs
+        console.log(toSparql(algebra));
+
+        //TODO: toSparql(algebra) an Ontologie schicken und Antwort als [expert] zurrückgeben
+
+        //Example
 		const expert = new Expert;
         expert.firstName = "Jens";
-		return [expert];
-	}
+        //Example ende
 
-    //how do i get the graphQL Query to use https://www.npmjs.com/package/graphql-to-sparql ????
+		return [expert];
+
+	}
 }
